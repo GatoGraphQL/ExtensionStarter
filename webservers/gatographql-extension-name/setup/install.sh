89ebd0ae2d2dd83e3d8ab635254858e120bc9cc3
Leonardo Losoviz <leo@getpop.org>
2023-09-22 14:38:51 +0800

    gatographql-extension-name => gatographql-extensions

--- webservers/gatographql-extension-name/setup/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-wp core install --url="gatographql-extension-name.lndo.site" --title="Gato GraphQL" --admin_user=admin --admin_password=admin --admin_email=admin@example.com --path=/app/wordpress 
-wp option update siteurl "https://gatographql-extension-name.lndo.site" --path=/app/wordpress 
-wp option update home "https://gatographql-extension-name.lndo.site" --path=/app/wordpress 
-# This change is needed on InstaWP
-# wp option update admin_email "admin@example.com" --path=/app/wordpress
