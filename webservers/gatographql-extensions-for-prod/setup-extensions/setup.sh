b236fb986cf5143c4b013c6d5e68a96ef6dff7f9
Leonardo Losoviz <leo@getpop.org>
2023-09-23 18:40:24 +0800

    Fixed setup-extensions

--- webservers/gatographql-extensions-for-prod/setup-extensions/setup.sh
+++ webservers/gatographql-extensions-for-prod/setup-extensions/setup.sh
@@ -1,6 +1,2 @@
 #!/bin/sh
-if wp core is-installed --path=/app/wordpress; then
-    echo "WordPress is already installed"
-    exit
-fi
-/bin/sh /app/setup/activate-plugins.sh
+/bin/sh /app/setup-extensions/activate-plugins.sh
