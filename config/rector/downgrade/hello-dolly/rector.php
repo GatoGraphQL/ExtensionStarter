3451ec3e0536ee61a9e987f0451051dc57346b12
Leonardo Losoviz <leo@getpop.org>
2023-09-25 15:38:28 +0800

    Fixed filename

--- config/rector/downgrade/hello-dolly/rector.php
+++ config/rector/downgrade/hello-dolly/rector.php
@@ -2,11 +2,11 @@
 
 declare(strict_types=1);
 
-use PoP\ExtensionStarter\Config\Rector\Downgrade\Configurators\HelloGatoContainerConfigurationService;
+use PoP\ExtensionStarter\Config\Rector\Downgrade\Configurators\HelloDollyContainerConfigurationService;
 use Rector\Config\RectorConfig;
 
 return static function (RectorConfig $rectorConfig): void {
-    $containerConfigurationService = new HelloGatoContainerConfigurationService(
+    $containerConfigurationService = new HelloDollyContainerConfigurationService(
         $rectorConfig,
         dirname(__DIR__, 4)
     );
