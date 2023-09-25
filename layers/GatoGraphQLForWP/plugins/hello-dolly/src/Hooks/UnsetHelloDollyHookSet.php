37fbbf89db342aa3e7c6aba1830bf7fe2960cc0c
Leonardo Losoviz <leo@getpop.org>
2023-09-25 15:28:58 +0800

    removeAction => remove_action

--- layers/GatoGraphQLForWP/plugins/hello-dolly/src/Hooks/UnsetHelloDollyHookSet.php
+++ layers/GatoGraphQLForWP/plugins/hello-dolly/src/Hooks/UnsetHelloDollyHookSet.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace DollyShepherd\HelloDolly\Hooks;
 
-use PoP\Root\App;
 use PoP\Root\Hooks\AbstractHookSet;
 
 class UnsetHelloDollyHookSet extends AbstractHookSet
@@ -12,7 +11,7 @@ class UnsetHelloDollyHookSet extends AbstractHookSet
     protected function init(): void
     {
         // As this is a sample plugin, remove the "Hello Dolly" notice in the admin
-        App::removeAction('admin_notices', 'hello_dolly');
-        App::removeAction('admin_head', 'dolly_css');
+        \remove_action('admin_notices', 'hello_dolly');
+        \remove_action('admin_head', 'dolly_css');
     }
 }
