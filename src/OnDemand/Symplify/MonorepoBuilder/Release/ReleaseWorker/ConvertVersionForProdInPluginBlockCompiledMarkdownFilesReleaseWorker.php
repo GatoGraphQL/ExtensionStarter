5b0080637b444d2ff9275790719a20d5c2d03de5
Leonardo Losoviz <leo@getpop.org>
2023-09-26 12:27:03 +0800

    MonorepoStaticHelpers

--- src/OnDemand/Symplify/MonorepoBuilder/Release/ReleaseWorker/ConvertVersionForProdInPluginBlockCompiledMarkdownFilesReleaseWorker.php
+++ src/OnDemand/Symplify/MonorepoBuilder/Release/ReleaseWorker/ConvertVersionForProdInPluginBlockCompiledMarkdownFilesReleaseWorker.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace PoP\ExtensionStarter\OnDemand\Symplify\MonorepoBuilder\Release\ReleaseWorker;
 
 use PoP\ExtensionStarter\Config\Symplify\MonorepoBuilder\DataSources\PluginDataSource;
+use PoP\ExtensionStarter\Monorepo\MonorepoStaticHelpers;
 use PoP\PoP\OnDemand\Symplify\MonorepoBuilder\Release\ReleaseWorker\ConvertVersionForProdInPluginBlockCompiledMarkdownFilesReleaseWorker as UpstreamConvertVersionForProdInPluginBlockCompiledMarkdownFilesReleaseWorker;
 
 class ConvertVersionForProdInPluginBlockCompiledMarkdownFilesReleaseWorker extends UpstreamConvertVersionForProdInPluginBlockCompiledMarkdownFilesReleaseWorker
@@ -12,5 +13,10 @@ class ConvertVersionForProdInPluginBlockCompiledMarkdownFilesReleaseWorker exten
     protected function getPluginDataSource(): PluginDataSource
     {
         return new PluginDataSource(dirname(__DIR__, 6));
+    }
+
+    protected function getGitHubRepoDocsRootURL(): string
+    {
+        return MonorepoStaticHelpers::getGitHubRepoDocsRootURL();
     }
 }
