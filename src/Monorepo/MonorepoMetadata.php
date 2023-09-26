3d8aaeecdcf445b4bdfbba5e2fca25f27b8792f8
Leonardo Losoviz <leo@getpop.org>
2023-09-26 12:41:04 +0800

    GITHUB_REPO_OWNER/NAME

--- src/Monorepo/MonorepoMetadata.php
+++ src/Monorepo/MonorepoMetadata.php
@@ -18,4 +18,7 @@ final class MonorepoMetadata
     final public const GIT_BASE_BRANCH = 'main';
     final public const GIT_USER_NAME = 'extension-git-user-name';
     final public const GIT_USER_EMAIL = 'extension-git-user@email.com';
+
+    final public const GITHUB_REPO_OWNER = 'ExtensionRepoOwner';
+    final public const GITHUB_REPO_NAME = 'ExtensionRepoName';
 }
