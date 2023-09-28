91e8264d8c147b071446e3307e9ee4303108641e
Leonardo Losoviz <leo@getpop.org>
2023-09-28 16:15:38 +0800

    Release => ModifyWorker

--- src/Config/Symplify/MonorepoBuilder/DataSources/ModifyProjectWorkersDataSource.php
+++ src/Config/Symplify/MonorepoBuilder/DataSources/ModifyProjectWorkersDataSource.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace PoP\ExtensionStarter\Config\Symplify\MonorepoBuilder\DataSources;
 
-use PoP\ExtensionStarter\OnDemand\Symplify\MonorepoBuilder\ModifyProject\ModifyProjectWorker\GuardOnDefaultBranchReleaseWorker;
-use PoP\ExtensionStarter\OnDemand\Symplify\MonorepoBuilder\ModifyProject\ModifyProjectWorker\UpdateCurrentBranchAliasReleaseWorker;
+use PoP\ExtensionStarter\OnDemand\Symplify\MonorepoBuilder\ModifyProject\ModifyProjectWorker\GuardOnDefaultBranchModifyProjectWorker;
+use PoP\ExtensionStarter\OnDemand\Symplify\MonorepoBuilder\ModifyProject\ModifyProjectWorker\UpdateCurrentBranchAliasModifyProjectWorker;
 
 class ModifyProjectWorkersDataSource
 {
@@ -15,13 +15,13 @@ class ModifyProjectWorkersDataSource
     public function getModifyProjectWorkerClasses(): array
     {
         return [
-            GuardOnDefaultBranchReleaseWorker::class,
+            GuardOnDefaultBranchModifyProjectWorker::class,
 
             /**
              * When doing a major release, the current alias must also be updated,
              * or otherwise there'll be conflicts with the "conflict" entries.
              */
-            UpdateCurrentBranchAliasReleaseWorker::class,
+            UpdateCurrentBranchAliasModifyProjectWorker::class,
         ];
     }
 }
