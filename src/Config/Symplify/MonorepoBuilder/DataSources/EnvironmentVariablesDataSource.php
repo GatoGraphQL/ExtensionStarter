665d1fb5be33c74e31838830aa8c84682e62283c
Leonardo Losoviz <leo@getpop.org>
2023-09-28 18:41:57 +0800

    use string for env vars

--- src/Config/Symplify/MonorepoBuilder/DataSources/EnvironmentVariablesDataSource.php
+++ src/Config/Symplify/MonorepoBuilder/DataSources/EnvironmentVariablesDataSource.php
@@ -17,7 +17,7 @@ class EnvironmentVariablesDataSource extends UpstreamEnvironmentVariablesDataSou
         return array_merge(
             parent::getEnvironmentVariables(),
             [
-                self::RETENTION_DAYS_FOR_GENERATED_PLUGINS => 90,
+                self::RETENTION_DAYS_FOR_GENERATED_PLUGINS => (string) 90,
                 self::GIT_BASE_BRANCH => MonorepoMetadata::GIT_BASE_BRANCH,
                 self::GIT_USER_NAME => MonorepoMetadata::GIT_USER_NAME,
                 self::GIT_USER_EMAIL => MonorepoMetadata::GIT_USER_EMAIL,
