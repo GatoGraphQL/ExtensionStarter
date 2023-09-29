e3a80e4b19724c3e6f832a52a77535e0d85fa7b4
Leonardo Losoviz <leo@getpop.org>
2023-09-29 15:48:12 +0800

    Removed GuardOnDefaultBranchModifyProjectWorker

--- src/Config/Symplify/MonorepoBuilder/DataSources/InitializeProjectWorkersDataSource.php
+++ src/Config/Symplify/MonorepoBuilder/DataSources/InitializeProjectWorkersDataSource.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace PoP\ExtensionStarter\Config\Symplify\MonorepoBuilder\DataSources;
 
-use PoP\ExtensionStarter\OnDemand\Symplify\MonorepoBuilder\ModifyProject\ModifyProjectWorker\GuardOnDefaultBranchModifyProjectWorker;
 use PoP\ExtensionStarter\OnDemand\Symplify\MonorepoBuilder\ModifyProject\ModifyProjectWorker\ReplaceMonorepoMetadataInitializeProjectWorker;
 
 class InitializeProjectWorkersDataSource
@@ -15,12 +14,6 @@ class InitializeProjectWorkersDataSource
     public function getInitializeProjectWorkerClasses(): array
     {
         return [
-            GuardOnDefaultBranchModifyProjectWorker::class,
-
-            /**
-             * When doing a major release, the current alias must also be updated,
-             * or otherwise there'll be conflicts with the "conflict" entries.
-             */
             ReplaceMonorepoMetadataInitializeProjectWorker::class,
         ];
     }
