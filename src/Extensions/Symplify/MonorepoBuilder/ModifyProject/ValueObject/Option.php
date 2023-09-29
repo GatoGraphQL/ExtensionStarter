05b342d999af76e6db2644c05f0a94e3ebeed45f
Leonardo Losoviz <leo@getpop.org>
2023-09-29 14:19:49 +0800

    Added Option

---
+++ src/Extensions/Symplify/MonorepoBuilder/ModifyProject/ValueObject/Option.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoP\ExtensionStarter\Extensions\Symplify\MonorepoBuilder\ModifyProject\ValueObject;
+
+final class Option
+{
+    /**
+     * @var string
+     */
+    public final const GITHUB_REPO_OWNER = 'github-repo-owner';
+    /**
+     * @var string
+     */
+    public final const GITHUB_REPO_NAME = 'github-repo-name';
+}
