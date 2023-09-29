5d62bf5892f58d8697d3033a2b8023687e197f32
Leonardo Losoviz <leo@getpop.org>
2023-09-29 14:19:43 +0800

    phpstan

--- src/Extensions/Symplify/MonorepoBuilder/ValueObject/Option.php
+++ src/Extensions/Symplify/MonorepoBuilder/ValueObject/Option.php
@@ -10,5 +10,8 @@ final class Option
      * @var string
      */
     public final const COPY_UPSTREAM_MONOREPO_FOLDER_ENTRIES = 'copy-upstream-monorepo-folder-entries';
+    /**
+     * @var string
+     */
     public final const COPY_UPSTREAM_MONOREPO_FILE_ENTRIES = 'copy-upstream-monorepo-file-entries';
 }
