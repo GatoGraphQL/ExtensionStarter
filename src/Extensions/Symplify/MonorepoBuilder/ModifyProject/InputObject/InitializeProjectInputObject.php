9b259743dff4c473ebb1049de0a395b49a2d0bdf
Leonardo Losoviz <leo@getpop.org>
2023-09-29 14:26:54 +0800

    methods on inputobject interface

--- src/Extensions/Symplify/MonorepoBuilder/ModifyProject/InputObject/InitializeProjectInputObject.php
+++ src/Extensions/Symplify/MonorepoBuilder/ModifyProject/InputObject/InitializeProjectInputObject.php
@@ -7,8 +7,18 @@ namespace PoP\ExtensionStarter\Extensions\Symplify\MonorepoBuilder\ModifyProject
 class InitializeProjectInputObject implements InitializeProjectInputObjectInterface
 {
     public function __construct(
-        public string $githubRepoOwner,
-        public string $githubRepoName,
+        private string $githubRepoOwner,
+        private string $githubRepoName,
     ){        
+    }
+
+    public function getGithubRepoOwner(): string
+    {
+        return $this->githubRepoOwner;
+    }
+
+    public function getGithubRepoName(): string
+    {
+        return $this->githubRepoName;
     }
 }
