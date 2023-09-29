47753a71fdaec405450c6718ea2d2beec5020c4e
Leonardo Losoviz <leo@getpop.org>
2023-09-29 17:15:51 +0800

    Metadata classes

---
+++ layers/GatoGraphQLForWP/plugins/hello-dolly/src/ExtensionMetadata.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DollyShepherd\HelloDolly;
+
+final class ExtensionMetadata
+{
+    final public const GIT_BASE_BRANCH = 'main';
+    final public const GITHUB_REPO_OWNER = 'GatoGraphQL';
+    final public const GITHUB_REPO_NAME = 'ExtensionStarter';
+}
