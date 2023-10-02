01226306646055dbdb4eda163b773ed6b4274278
Leonardo Losoviz <leo@getpop.org>
2023-10-02 15:33:08 +0800

    Added consts

--- src/Extensions/Symplify/MonorepoBuilder/ModifyProject/ValueObject/Option.php
+++ src/Extensions/Symplify/MonorepoBuilder/ModifyProject/ValueObject/Option.php
@@ -14,4 +14,12 @@ final class Option
      * @var string
      */
     public final const GITHUB_REPO_NAME = 'github-repo-name';
+    /**
+     * @var string
+     */
+    public final const DOCS_GITHUB_REPO_OWNER = 'docs-github-repo-owner';
+    /**
+     * @var string
+     */
+    public final const DOCS_GITHUB_REPO_NAME = 'docs-github-repo-name';
 }
