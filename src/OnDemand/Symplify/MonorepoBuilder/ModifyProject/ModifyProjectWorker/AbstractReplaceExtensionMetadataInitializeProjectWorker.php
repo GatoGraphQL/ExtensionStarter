d8ea6e10086571ef3a719011ca31f95b957c9ff8
Leonardo Losoviz <leo@getpop.org>
2023-10-02 08:21:39 +0800

    ReplaceExtensionMetadataInitializeProjectWorker

---
+++ src/OnDemand/Symplify/MonorepoBuilder/ModifyProject/ModifyProjectWorker/AbstractReplaceExtensionMetadataInitializeProjectWorker.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoP\ExtensionStarter\OnDemand\Symplify\MonorepoBuilder\ModifyProject\ModifyProjectWorker;
+
+use PoP\ExtensionStarter\Config\Symplify\MonorepoBuilder\DataSourceAccessors\PluginDataSourceAccessor;
+use PoP\ExtensionStarter\Extensions\Symplify\MonorepoBuilder\ModifyProject\Contract\ModifyProjectWorker\InitializeProjectWorkerInterface;
+use PoP\PoP\Config\Symplify\MonorepoBuilder\DataSources\PluginDataSource;
+use PoP\PoP\Extensions\Symplify\MonorepoBuilder\SmartFile\FileContentReplacerSystem;
+
+abstract class AbstractReplaceExtensionMetadataInitializeProjectWorker implements InitializeProjectWorkerInterface
+{
+    /** @var string[]|null */
+    private ?array $extensionSrcMetadataFiles = null;
+
+    public function __construct(
+        protected FileContentReplacerSystem $fileContentReplacerSystem,
+    ) {
+    }
+
+    /**
+     * @return string[]
+     */
+    protected function getExtensionSrcMetadataFiles(): array
+    {
+        if ($this->extensionSrcMetadataFiles === null) {
+            $pluginDataSource = $this->getPluginDataSource();
+            $pluginDataSourceAccessor = new PluginDataSourceAccessor($pluginDataSource);
+            $this->extensionSrcMetadataFiles = $pluginDataSourceAccessor->getExtensionSrcMetadataFiles();
+        }
+        return $this->extensionSrcMetadataFiles;
+    }
+
+    protected function getPluginDataSource(): PluginDataSource
+    {
+        return new PluginDataSource(dirname(__DIR__, 6));
+    }
+}
---
+++ src/OnDemand/Symplify/MonorepoBuilder/ModifyProject/ModifyProjectWorker/ReplaceExtensionMetadataInitializeProjectWorker.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoP\ExtensionStarter\OnDemand\Symplify\MonorepoBuilder\ModifyProject\ModifyProjectWorker;
+
+use PoP\ExtensionStarter\Extensions\Symplify\MonorepoBuilder\ModifyProject\InputObject\InitializeProjectInputObjectInterface;
+use PoP\ExtensionStarter\Extensions\Symplify\MonorepoBuilder\ModifyProject\InputObject\ModifyProjectInputObjectInterface;
+
+class ReplaceExtensionMetadataInitializeProjectWorker extends AbstractReplaceExtensionMetadataInitializeProjectWorker
+{
+    use ReplaceMetadataInitializeProjectWorkerTrait;
+
+    /**
+     * @param InitializeProjectInputObjectInterface $inputObject
+     */
+    public function work(ModifyProjectInputObjectInterface $inputObject): void
+    {
+        $files = $this->getExtensionSrcMetadataFiles();
+        $this->replaceMetadataInFiles($inputObject, $files);
+    }
+
+    /**
+     * @param InitializeProjectInputObjectInterface $inputObject
+     */
+    public function getDescription(ModifyProjectInputObjectInterface $inputObject): string
+    {
+        return 'Replace all properties in the ExtensionMetadata file';
+    }
+}
