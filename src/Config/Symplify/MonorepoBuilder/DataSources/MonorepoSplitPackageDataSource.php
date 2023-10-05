5f03759b2bf9003f9d64379a4b12d42412436e22
leoloso <leo@getpop.org>
2023-10-05 16:04:56 +0800

    gatographql-extension-starter monorepo split

--- src/Config/Symplify/MonorepoBuilder/DataSources/MonorepoSplitPackageDataSource.php
+++ src/Config/Symplify/MonorepoBuilder/DataSources/MonorepoSplitPackageDataSource.php
@@ -21,8 +21,22 @@ class MonorepoSplitPackageDataSource extends UpstreamMonorepoSplitPackageDataSou
      */
     public function getSkipMonorepoSplitPackagePaths(): array
     {
+        /**
+         * @gatographql-extension-starter When pushing code to the repo, the "monorepo split" feature
+         *                                copies all code for each of the modified packages into their
+         *                                own GitHub repo. (Eg: package "hello-dolly-schema" could be
+         *                                pushed to http://github.com/GatoGraphQL/hello-dolly-schema)
+         *                                This feature:
+         *                                - is useful for distributing packages via Composer
+         *                                - allows exploring their source code outside of the monorepo
+         *                                Otherwise, it is not needed for creating a Gato GraphQL
+         *                                extension plugin (hence it's currently disabled for all packages).
+         *                                Adding the paths to the packages disables doing the "monorepo split"
+         *                                on those packages.
+         */
         $packagePaths = [
-            'layers/',
+            'layers/GatoGraphQLForWP/packages/',
+            'layers/GatoGraphQLForWP/plugins/',
         ];
 
         if ($this->includeUpstreamPackages) {
