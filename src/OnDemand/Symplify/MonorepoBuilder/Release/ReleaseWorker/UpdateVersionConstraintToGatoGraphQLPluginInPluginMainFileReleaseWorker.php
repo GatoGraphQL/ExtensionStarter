9ba17f9cfb40ced223ab089733637694a30a051a
leoloso <leo@getpop.org>
2023-10-07 07:16:27 +0800

    add PluginDataSource

--- src/OnDemand/Symplify/MonorepoBuilder/Release/ReleaseWorker/UpdateVersionConstraintToGatoGraphQLPluginInPluginMainFileReleaseWorker.php
+++ src/OnDemand/Symplify/MonorepoBuilder/Release/ReleaseWorker/UpdateVersionConstraintToGatoGraphQLPluginInPluginMainFileReleaseWorker.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace PoP\ExtensionStarter\OnDemand\Symplify\MonorepoBuilder\Release\ReleaseWorker;
 
 use PharIo\Version\Version;
+use PoP\ExtensionStarter\Config\Symplify\MonorepoBuilder\DataSources\PluginDataSource;
 use PoP\PoP\Monorepo\MonorepoMetadata as UpstreamMonorepoMetadata;
 use PoP\PoP\OnDemand\Symplify\MonorepoBuilder\Release\ReleaseWorker\AbstractConvertVersionInPluginMainFileReleaseWorker;
 
@@ -19,7 +20,7 @@ class UpdateVersionConstraintToGatoGraphQLPluginInPluginMainFileReleaseWorker ex
         $requiredGatoGraphQLPluginVersion = $this->versionUtils->getRequiredCurrentFormat(UpstreamMonorepoMetadata::LATEST_PROD_VERSION);
 
         $replacements = [
-            "/" . preg_quote('$gatoGraphQLPluginVersionConstraint') . " = '[0-9.^]+';/" => "\$gatoGraphQLPluginVersionConstraint = '$requiredGatoGraphQLPluginVersion';",
+            "/" . preg_quote('$gatoGraphQLPluginVersionConstraint') . " = '.*';/" => "\$gatoGraphQLPluginVersionConstraint = '$requiredGatoGraphQLPluginVersion';",
         ];
         $this->fileContentReplacerSystem->replaceContentInFiles($this->getPluginMainFiles(), $replacements, true);
     }
@@ -27,5 +28,10 @@ class UpdateVersionConstraintToGatoGraphQLPluginInPluginMainFileReleaseWorker ex
     public function getDescription(Version $version): string
     {
         return 'Update the constraint to the Gato GraphQL plugin to its latest PROD version, in the plugin main file';
+    }
+    
+    protected function getPluginDataSource(): PluginDataSource
+    {
+        return new PluginDataSource(dirname(__DIR__, 6));
     }
 }
