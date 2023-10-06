74663b79bfa29daa40a5a5ffda2687474bb8f5fb
leoloso <leo@getpop.org>
2023-10-06 15:13:08 +0800

    `@gatographql-project-info`

--- src/Config/Symplify/MonorepoBuilder/DataSources/SkipDowngradeTestPathsDataSource.php
+++ src/Config/Symplify/MonorepoBuilder/DataSources/SkipDowngradeTestPathsDataSource.php
@@ -25,8 +25,26 @@ class SkipDowngradeTestPathsDataSource extends UpstreamSkipDowngradeTestPathsDat
                 fn (string $path) => $this->upstreamRelativeRootPath . '/' . $path,
                 parent::getSkipDowngradeTestProjectPaths()
             ),
-            // @todo Complete for Hello Dolly!
-            []
+            $this->getExtensionSkipDowngradeTestProjectPaths()
         );
+    }
+
+    /**
+     * @gatographql-project-info
+     *
+     * It there are packages in the monorepo that will not be
+     * included in the plugin, then they need not be downgraded.
+     * 
+     * Then list their paths here, as to avoid downgrading them.
+     *
+     * For instance, packages for powering PHPUnit tests,
+     * under 'layers/GatoGraphQLForWP/phpunit-packages/',
+     * can be skipped.
+     * 
+     * @return string[]
+     */
+    protected function getExtensionSkipDowngradeTestProjectPaths(): array
+    {
+        return [];
     }
 }
