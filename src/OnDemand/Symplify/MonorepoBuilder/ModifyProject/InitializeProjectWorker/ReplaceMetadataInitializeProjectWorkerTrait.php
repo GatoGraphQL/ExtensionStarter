0c61f13632fee4e335c2c00ef8929ceadab6c7e7
leoloso <leo@getpop.org>
2023-11-07 15:43:38 +0800

    Moved classes to InitializeProjectWorker

--- src/OnDemand/Symplify/MonorepoBuilder/ModifyProject/InitializeProjectWorker/ReplaceMetadataInitializeProjectWorkerTrait.php
+++ src/OnDemand/Symplify/MonorepoBuilder/ModifyProject/InitializeProjectWorker/ReplaceMetadataInitializeProjectWorkerTrait.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace PoP\ExtensionStarter\OnDemand\Symplify\MonorepoBuilder\ModifyProject\ModifyProjectWorker;
+namespace PoP\ExtensionStarter\OnDemand\Symplify\MonorepoBuilder\ModifyProject\InitializeProjectWorker;
 
 use PoP\ExtensionStarter\Extensions\Symplify\MonorepoBuilder\ModifyProject\InputObject\InitializeProjectInputObjectInterface;
 
