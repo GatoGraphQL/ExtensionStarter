303e9f243d3ad40b8012ee695fde62f6b678cc0d
leoloso <leo@getpop.org>
2023-11-07 13:53:36 +0800

    slugify

--- src/Extensions/Symplify/MonorepoBuilder/Utils/StringUtils.php
+++ src/Extensions/Symplify/MonorepoBuilder/Utils/StringUtils.php
@@ -35,4 +35,34 @@ final class StringUtils
 
         return $str;
     }
+
+    /**
+     * @see https://stackoverflow.com/a/2955878/14402031
+     */
+    public function slugify(string $text, string $divider = '-'): string
+    {
+        // replace non letter or digits by divider
+        $text = preg_replace('~[^\pL\d]+~u', $divider, $text);
+
+        // transliterate
+        $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+
+        // remove unwanted characters
+        $text = preg_replace('~[^-\w]+~', '', $text);
+
+        // trim
+        $text = trim($text, $divider);
+
+        // remove duplicate divider
+        $text = preg_replace('~-+~', $divider, $text);
+
+        // lowercase
+        $text = strtolower($text);
+
+        if (empty($text)) {
+            return 'n-a';
+        }
+
+        return $text;
+    }
 }
