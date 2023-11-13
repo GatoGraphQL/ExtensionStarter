75d66ea8545260f0743627de42ac6c8a6197315a
leoloso <leo@getpop.org>
2023-11-13 20:09:53 +0800

    added missing ModuleTest for HelloDolly

---
+++ layers/GatoGraphQLForWP/packages/hello-dolly-schema/tests/ModuleTest.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MyCompanyForGatoGraphQL\HelloDollySchema;
+
+use PoP\ComponentModel\AbstractModuleTestCase;
+
+class ModuleTest extends AbstractModuleTestCase
+{
+}
