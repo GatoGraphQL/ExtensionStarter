e154e18feba3b205e1b3f2f83f196afbf9541c2b
leoloso <leo@getpop.org>
2023-11-21 13:40:56 +0800

    getAdditionalDowngradeRectorBeforeFiles

--- src/Config/Symplify/MonorepoBuilder/DataSources/DowngradeRectorDataSource.php
+++ src/Config/Symplify/MonorepoBuilder/DataSources/DowngradeRectorDataSource.php
@@ -18,6 +18,16 @@ class DowngradeRectorDataSource extends UpstreamDowngradeRectorDataSource
     /**
      * @return string[]
      */
+    public function getAdditionalDowngradeRectorBeforeFiles(): array
+    {
+        return [
+            $this->rootDir . '/' . $this->upstreamRelativeRootPath . '/config/rector/downgrade/monorepo/chained-rules/rector-change-if-or-return-earlyreturn.php',
+        ];
+    }
+
+    /**
+     * @return string[]
+     */
     public function getAdditionalDowngradeRectorAfterFiles(): array
     {
         return [
