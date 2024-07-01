b7630d0b454133e9feaedafd015184b96a0e9375
leoloso <leo@getpop.org>
2024-07-01 20:14:16 +0800

    open 2.7-dev

--- src/Monorepo/MonorepoMetadata.php
+++ src/Monorepo/MonorepoMetadata.php
@@ -26,7 +26,7 @@ final class MonorepoMetadata
      *
      * @gatographql-readonly-code
      */
-    final public const VERSION = '2.6.0';
+    final public const VERSION = '2.7.0-dev';
 
     /**
      * @gatographql-project-info
