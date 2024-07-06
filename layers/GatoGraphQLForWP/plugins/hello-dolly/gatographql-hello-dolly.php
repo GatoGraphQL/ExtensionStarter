97d34d705668ee3be1b82b1d4d2d7ad35d3de763
leoloso <leo@getpop.org>
2024-07-06 13:32:04 +0800

    Requires at least: 6.0

--- layers/GatoGraphQLForWP/plugins/hello-dolly/gatographql-hello-dolly.php
+++ layers/GatoGraphQLForWP/plugins/hello-dolly/gatographql-hello-dolly.php
@@ -4,7 +4,7 @@ Plugin Name: Gato GraphQL - Hello Dolly
 Plugin URI:
 Description: Integration of plugin Hello Dolly with Gato GraphQL
 Version: 2.7.0-dev
-Requires at least: 5.4
+Requires at least: 6.0
 Requires PHP: 8.1
 Author: My Company
 License:
