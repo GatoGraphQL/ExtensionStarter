93cbff2b90d16cfbc377b6bfd3e3b68e3f829051
leoloso <leo@getpop.org>
2024-07-10 07:42:16 +0800

    open 3.1-dev

--- layers/GatoGraphQLForWP/plugins/hello-dolly/gatographql-hello-dolly.php
+++ layers/GatoGraphQLForWP/plugins/hello-dolly/gatographql-hello-dolly.php
@@ -3,7 +3,7 @@
 Plugin Name: Gato GraphQL - Hello Dolly
 Plugin URI:
 Description: Integration of plugin Hello Dolly with Gato GraphQL
-Version: 3.0.0
+Version: 3.1.0-dev
 Requires at least: 6.0
 Requires PHP: 8.1
 Author: My Company
@@ -50,7 +50,7 @@ add_action(
          *
          * @gatographql-readonly-code
          */
-        $extensionVersion = '3.0.0';
+        $extensionVersion = '3.1.0-dev';
         $extensionName = \__('Gato GraphQL - Hello Dolly', 'gatographql-hello-dolly');
         /**
          * @gatographql-extension-info
--- src/Monorepo/MonorepoMetadata.php
+++ src/Monorepo/MonorepoMetadata.php
@@ -26,7 +26,7 @@ final class MonorepoMetadata
      *
      * @gatographql-readonly-code
      */
-    final public const VERSION = '3.0.0';
+    final public const VERSION = '3.1.0-dev';
 
     /**
      * @gatographql-project-info
