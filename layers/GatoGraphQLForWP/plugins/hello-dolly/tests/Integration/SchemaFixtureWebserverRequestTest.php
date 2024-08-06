1061f7921dc890c4b763e7f8c65173d532a5c1c5
leoloso <leo@getpop.org>
2024-08-06 14:59:37 +0800

    Migrated to AppPassword

--- layers/GatoGraphQLForWP/plugins/hello-dolly/tests/Integration/SchemaFixtureWebserverRequestTest.php
+++ layers/GatoGraphQLForWP/plugins/hello-dolly/tests/Integration/SchemaFixtureWebserverRequestTest.php
@@ -4,12 +4,17 @@ declare(strict_types=1);
 
 namespace MyCompanyForGatoGraphQL\HelloDolly\Integration;
 
-use PHPUnitForGatoGraphQL\GatoGraphQL\Integration\AbstractAdminClientQueryExecutionFixtureWebserverRequestTestCase;
+use PHPUnitForGatoGraphQL\GatoGraphQL\Integration\AbstractApplicationPasswordQueryExecutionFixtureWebserverRequestTestCase;
 
-class SchemaFixtureWebserverRequestTest extends AbstractAdminClientQueryExecutionFixtureWebserverRequestTestCase
+class SchemaFixtureWebserverRequestTest extends AbstractApplicationPasswordQueryExecutionFixtureWebserverRequestTestCase
 {
     protected static function getFixtureFolder(): string
     {
         return __DIR__ . '/fixture-schema';
+    }
+
+    protected static function getEndpoint(): string
+    {
+        return 'graphql/';
     }
 }
