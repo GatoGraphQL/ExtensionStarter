81a318045b2b0586bae2c439282a9429357d0056
leoloso <leo@getpop.org>
2024-10-16 15:10:27 +0800

    Renamed to maybeAdaptGatoGraphQLBundledExtensionPluginFile

--- layers/GatoGraphQLForWP/plugins/hello-dolly/gatographql-hello-dolly.php
+++ layers/GatoGraphQLForWP/plugins/hello-dolly/gatographql-hello-dolly.php
@@ -176,7 +176,7 @@ add_action(
 
         // Create and set-up the extension instance
         $extensionManager->register(new GatoGraphQLExtension(
-            getGatoGraphQLBundledExtensionExpectedPluginFile(
+            maybeAdaptGatoGraphQLBundledExtensionPluginFile(
                 __FILE__,
                 GatoGraphQLExtension::class,
                 'my-company-for-gatographql',
--- templates/basic/layers/GatoGraphQLForWP/plugins/extension-template/gatographql-extension-template.php
+++ templates/basic/layers/GatoGraphQLForWP/plugins/extension-template/gatographql-extension-template.php
@@ -180,7 +180,7 @@ add_action(
 
         // Create and set-up the extension instance
         $extensionManager->register(new GatoGraphQLExtension(
-            getGatoGraphQLBundledExtensionExpectedPluginFile(
+            maybeAdaptGatoGraphQLBundledExtensionPluginFile(
                 __FILE__,
                 GatoGraphQLExtension::class,
                 'my-company-for-gatographql',
