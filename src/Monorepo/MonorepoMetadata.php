dc3362198bf111a4df33874c162a4f84769ec5bc
leoloso <leo@getpop.org>
2024-11-30 09:17:34 +0800

    prepare release

--- src/Monorepo/MonorepoMetadata.php
+++ src/Monorepo/MonorepoMetadata.php
@@ -26,7 +26,7 @@ final class MonorepoMetadata
      *
      * @gatographql-readonly-code
      */
-    final public const VERSION = '7.1.0-dev';
+    final public const VERSION = '8.0.0';
 
     /**
      * @gatographql-project-info
@@ -40,7 +40,7 @@ final class MonorepoMetadata
      *
      * @gatographql-readonly-code
      */
-    final public const LATEST_PROD_VERSION = '7.0.8';
+    final public const LATEST_PROD_VERSION = '8.0.0';
 
     /**
      * @gatographql-project-info
