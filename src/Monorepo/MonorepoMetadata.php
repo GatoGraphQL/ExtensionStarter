83deb960debd36a31f3128f4416e9a159262c5d8
leoloso <leo@getpop.org>
2025-04-19 17:08:01 +0800

    open 11.4-dev

--- src/Monorepo/MonorepoMetadata.php
+++ src/Monorepo/MonorepoMetadata.php
@@ -26,7 +26,7 @@ final class MonorepoMetadata
      *
      * @gatographql-readonly-code
      */
-    final public const VERSION = '11.3.1';
+    final public const VERSION = '11.4.0-dev';
 
     /**
      * @gatographql-project-info
