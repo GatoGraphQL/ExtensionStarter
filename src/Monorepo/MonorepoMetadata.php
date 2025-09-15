9af50cca2af52b16a649b315b2f4dba76d148098
leoloso <leo@getpop.org>
2025-09-15 15:27:55 +0800

    open 14.1-dev

--- src/Monorepo/MonorepoMetadata.php
+++ src/Monorepo/MonorepoMetadata.php
@@ -26,7 +26,7 @@ final class MonorepoMetadata
      *
      * @gatographql-readonly-code
      */
-    final public const VERSION = '14.0.2';
+    final public const VERSION = '14.1.0-dev';
 
     /**
      * @gatographql-project-info
